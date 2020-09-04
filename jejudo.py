23f02c6ab661014d8f98860e16b38361ca39c032
SEUNG BEEN LEE <sbstevenlee@gmail.com>
2020-09-04 16:54:03 +0900

    Rename Jejudo.py to jejudo.py

--- jejudo.py
+++ jejudo.py
@@ -1,5 +1,40 @@
 import pandas as pd
 
+def concat(jjd1, jjd2):
+    jjd3 = Jejudo()
+
+    df1 = jjd1.asv_table
+    df2 = jjd2.asv_table
+    df1["Sequence"] = jjd1.tax_table["Sequence"]
+    df2["Sequence"] = jjd2.tax_table["Sequence"]
+    df3 = pd.concat([df1, df2], ignore_index=True).fillna(0)
+    asv_table = df3.groupby("Sequence").sum()
+
+    df1 = jjd1.tax_table
+    df2 = jjd2.tax_table
+    df3 = pd.concat([df1, df2], ignore_index=True)
+    df3 = df3.drop_duplicates(subset=["Sequence"])
+    df3 = df3.sort_values("Sequence")
+    tax_table = df3.set_index("Sequence")
+
+    df1 = jjd1.sample_data
+    df2 = jjd2.sample_data
+    df1 = df1.reset_index()
+    df2 = df2.reset_index()
+    df3 = pd.concat([df1, df2], ignore_index=True)
+    sample_data = df3.set_index("Sample")
+
+    asv_list = [f"ASV{x+1}" for x in range(asv_table.shape[0])]
+    asv_table.index = asv_list
+    tax_table["Sequence"] = tax_table.index
+    tax_table.index = asv_list
+
+    jjd3.asv_table = asv_table
+    jjd3.tax_table = tax_table
+    jjd3.sample_data = sample_data
+
+    return jjd3
+
 class Jejudo:
     def __init__(self):
         self._asv_table = pd.DataFrame()
