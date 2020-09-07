bbcd8a698a93365f42fe180e93685681b6cb24a2
SEUNG BEEN LEE <sbstevenlee@gmail.com>
2020-09-07 10:54:42 +0900

    Update jejudo.py

--- jejudo.py
+++ jejudo.py
@@ -1,70 +1,135 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import decomposition
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import KMeans
-from sklearn.manifold import TSNE
+import sklearn
+import copy
 
-class Udo:
-    def __init__(self):
-        self._method = None
-        self._embedding = None
-        self._X = None
+def transform(jjd1, method):
+    jjd2 = copy.deepcopy(jjd1)
+    df = jjd2.asv_table
 
-    @property
-    def method(self):
-        return self._method
+    if method == 'z':
+        df = (df - df.mean()) / df.std()
 
-    @method.setter
-    def method(self, x):
-        self._method = x
+    elif method == 'p':
+        df = df / df.sum()
 
-    @property
-    def embedding(self):
-        return self._embedding
+    jjd2.asv_table = df
 
-    @embedding.setter
-    def embedding(self, x):
-        self._embedding = x
+    return jjd2
 
-    @property
-    def X(self):
-        return self._X
-
-    @X.setter
-    def X(self, x):
-        self._X = x
-
-def ordinate(jjd, method):
-    ud = Udo()
-
-    df = jjd.asv_table.T
+def ordinate(jejudo, method, n_components=2):
+    udo = Udo()
+    df = jejudo.asv_table.T
 
     if method == 'TSNE':
-        embedding = TSNE(n_components=2)
+        embedding = sklearn.manifold.TSNE(n_components)
         X = embedding.fit_transform(df)
 
-    ud.X = X
-    ud.method = method
-    ud.embedding = embedding
+    elif method == 'PCA':
+        embedding = sklearn.decomposition.PCA(n_components)
+        X = embedding.fit_transform(df)
 
-    return ud
+    elif method == 'MDS':
+        embedding = sklearn.manifold.MDS(n_components)
+        X = embedding.fit_transform(df)
 
-def plot_ordination(jjd, ud, feat):
-    fig, ax = plt.subplots(figsize=(7,7))
+    udo.method = method
+    udo.embedding = embedding
+    udo.X = X
 
-    a = jjd.smp_table[feat].unique()
-    color_map = dict(zip(a, [None for x in a]))
+    return udo
 
-    color_map = dict(sorted(color_map.items()))
-    for k, v in color_map.items():
-        i = jjd.smp_table[feat] == k
-        ax.scatter(ud.X[i, 0], ud.X[i, 1], label=k, color=v, s=90)
+def plot_ordination(jejudo, udo, feature=None, elev=10, azim=30, figsize=(7,7)):
+    n_components = udo.X.shape[1]
 
-    ax.set_xlabel(f"{ud.method} 1")
-    ax.set_ylabel(f"{ud.method} 2")
-    ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+    if n_components == 2:
+        fig, ax = plt.subplots(figsize=figsize)
+        ax.set_xlabel('1D')
+        ax.set_ylabel('2D')
+    elif n_components == 3:
+        fig = plt.figure(1, figsize=figsize)
+        ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=elev, azim=azim)
+        ax.set_xlabel('1D')
+        ax.set_ylabel('2D')
+        ax.set_zlabel('3D')
+    else:
+        raise ValueError("Incorrect dimension detected")
 
+    ax.set_title(f"{udo.method}")
+
+    if feature:
+        a = jejudo.smp_table[feature].unique()
+        color_map = dict(zip(a, [None for x in a]))
+        color_map = dict(sorted(color_map.items()))
+        for k, v in color_map.items():
+            i = jejudo.smp_table[feature] == k
+            ax.scatter(udo.X[i, 0], udo.X[i, 1], label=k, color=v, s=90)
+            ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+
+    else:
+        ax.scatter(udo.X[:, 0], udo.X[:, 1], s=90)
+
+def plot_importance(jejudo, udo, n_tax=10):
+    n_components = udo.embedding.components_.shape[0]
+
+    a = {'ASV': jejudo.asv_table.index,
+         '1D': udo.embedding.components_[0],
+         '2D': udo.embedding.components_[1]}
+
+    if n_components == 3:
+        a['3D'] = udo.embedding.components_[2]
+
+    df = pd.DataFrame(a)
+    df = df.set_index('ASV')
+
+    if n_components == 2:
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8,5))
+    else:
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10,5))
+
+    pc1 = df.abs().sort_values('1D', ascending=False)
+    pc2 = df.abs().sort_values('2D', ascending=False)
+    if n_components == 3:
+        pc3 = df.abs().sort_values('3D', ascending=False)
+
+    n_tax = 10
+
+    ax1.bar(pc1.index[:n_tax], pc1['1D'][:n_tax])
+    ax2.bar(pc2.index[:n_tax], pc2['2D'][:n_tax])
+    if n_components == 3:
+        ax3.bar(pc3.index[:n_tax], pc3['3D'][:n_tax])
+
+    ax1.set_title('1D')
+    ax2.set_title('2D')
+    if n_components == 3:
+        ax3.set_title('3D')
+
+    ax1.set_ylabel('Feature Importance')
+
+    ax1.tick_params(axis='x', rotation=90)
+    ax2.tick_params(axis='x', rotation=90)
+    if n_components == 3:
+        ax3.tick_params(axis='x', rotation=90)
+
+    fig.tight_layout()
+    plt.show()
+
+    ps1 = jejudo.tax_table.loc[ pc1.index[:n_tax] , : ]
+    ps2 = jejudo.tax_table.loc[ pc2.index[:n_tax] , : ]
+    if n_components == 3:
+        ps3 = jejudo.tax_table.loc[ pc3.index[:n_tax] , : ]
+
+    ps1.insert(0, 'Loading', df.loc[pc1.index[:n_tax], '1D'])
+    ps2.insert(0, 'Loading', df.loc[pc2.index[:n_tax], '2D'])
+    if n_components == 3:
+        ps3.insert(0, 'Loading', df.loc[pc3.index[:n_tax], '3D'])
+
+    display(ps1)
+    display(ps2)
+    if n_components == 3:
+        display(ps3)
 
 def concat(jjd1, jjd2):
     jjd3 = Jejudo()
@@ -109,6 +174,36 @@ def concat(jjd1, jjd2):
     jjd3.seq_table = seq_df
 
     return jjd3
+
+class Udo:
+    def __init__(self):
+        self._method = None
+        self._embedding = None
+        self._X = None
+
+    @property
+    def method(self):
+        return self._method
+
+    @method.setter
+    def method(self, x):
+        self._method = x
+
+    @property
+    def embedding(self):
+        return self._embedding
+
+    @embedding.setter
+    def embedding(self, x):
+        self._embedding = x
+
+    @property
+    def X(self):
+        return self._X
+
+    @X.setter
+    def X(self, x):
+        self._X = x
 
 class Jejudo:
     def __init__(self):
@@ -193,185 +288,6 @@ class Jejudo:
         df['Target'] = a.agg(':'.join, axis=1)
         df = df.groupby('Target').sum()
         return df
-
-    def pca2d(self, var, color_map=None):
-        result = {}
-
-        df = self.asv_table
-        df = (df - df.mean()) / df.std()
-        df = df.T
-
-        fig, ax = plt.subplots(figsize=(7,7))
-        pca = decomposition.PCA(2)
-        X = pca.fit_transform(df)
-
-        # Store the percentage of variance explained by each PC.
-        a = pca.explained_variance_ratio_
-
-        summary = ["Variance explained by PC 1: %.2f" % a[0],
-                   "Variance explained by PC 2: %.2f" % a[1],
-                   "Total variance explained: %.2f" % (a[0] + a[1])]
-
-        result['summary'] = '\n'.join(summary)
-
-        # Store the PCA plot.
-        if not color_map:
-            a = self.smp_table[var].unique()
-            color_map = dict(zip(a, [None for x in a]))
-
-        color_map = dict(sorted(color_map.items()))
-
-        for k, v in color_map.items():
-            i = self.smp_table[var] == k
-            ax.scatter(X[i, 0], X[i, 1], label=k, color=v, s=90)
-
-        ax.set_xlabel('PC 1')
-        ax.set_ylabel('PC 2')
-        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-        plt.close()
-        result['plot1'] = fig
-
-        # Store the feature importance plot.
-        fig, (ax1, ax2) = plt.subplots(1, 2)
-
-        a = {'ASV': df.columns, 'PC1': pca.components_[0], 'PC2': pca.components_[1]}
-        df = pd.DataFrame(a)
-        df = df.set_index('ASV')
-
-        pc1 = df.abs().sort_values('PC1', ascending=False)
-        pc2 = df.abs().sort_values('PC2', ascending=False)
-
-        n = 10
-
-        ax1.bar(pc1.index[:n], pc1['PC1'][:n])
-        ax2.bar(pc2.index[:n], pc2['PC2'][:n])
-
-        ax1.set_title('PC 1')
-        ax2.set_title('PC 2')
-
-        ax1.set_ylabel('Feature Importance')
-        ax2.set_ylabel('Feature Importance')
-
-        ax1.tick_params(axis='x', rotation=90)
-        ax2.tick_params(axis='x', rotation=90)
-
-        fig.tight_layout()
-        plt.close()
-        result['plot2'] = fig
-
-        # Store the top ASV loadings for each PC.
-        pd.set_option('display.max_colwidth', None)
-
-        ps1 = self.tax_table.loc[ pc1.index[:n] , : ]
-        ps2 = self.tax_table.loc[ pc2.index[:n] , : ]
-
-        ps1.insert(0, 'Loading', df.loc[pc1.index[:n], 'PC1'])
-        ps2.insert(0, 'Loading', df.loc[pc2.index[:n], 'PC2'])
-
-        result['loading1'] = ps1
-        result['loading2'] = ps2
-
-        return result
-
-    def pca3d(self, var, color_map=None, elev=10, azim=30):
-        result = {}
-
-        df = self.asv_table
-        df = (df - df.mean()) / df.std()
-        df = df.T
-
-        fig = plt.figure(1, figsize=(7, 7))
-        plt.clf()
-        ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=elev, azim=azim)
-        plt.cla()
-
-        pca = decomposition.PCA(n_components=3)
-        pca.fit(df)
-        X = pca.transform(df)
-
-        # Store the percentage of variance explained by each PC.
-        a = pca.explained_variance_ratio_
-
-        summary = ["Variance explained by PC 1: %.2f" % a[0],
-                   "Variance explained by PC 2: %.2f" % a[1],
-                   "Variance explained by PC 3: %.2f" % a[2],
-                   "Total variance explained: %.2f" % (a[0] + a[1] + a[2])]
-
-        result['summary'] = '\n'.join(summary)
-
-        # Store the PCA plot.
-        if not color_map:
-            a = self.smp_table[var].unique()
-            color_map = dict(zip(a, [None for x in a]))
-
-        color_map = dict(sorted(color_map.items()))
-
-        for k, v in color_map.items():
-            i = self.smp_table[var] == k
-            ax.scatter(X[i, 0], X[i, 1], X[i, 2], label=k, color=v, s=80)
-
-        ax.set_xlabel('PC 1')
-        ax.set_ylabel('PC 2')
-        ax.set_zlabel('PC 3')
-        ax.w_xaxis.set_ticklabels([])
-        ax.w_yaxis.set_ticklabels([])
-        ax.w_zaxis.set_ticklabels([])
-        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-        plt.close()
-        result['plot1'] = fig
-
-        # Store the feature importance plot.
-        fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
-
-        a = {'ASV': df.columns, 'PC1': pca.components_[0],
-             'PC2': pca.components_[1], 'PC3': pca.components_[2]}
-        df = pd.DataFrame(a)
-        df = df.set_index('ASV')
-
-        pc1 = df.abs().sort_values('PC1', ascending=False)
-        pc2 = df.abs().sort_values('PC2', ascending=False)
-        pc3 = df.abs().sort_values('PC3', ascending=False)
-
-        n = 10
-
-        ax1.bar(pc1.index[:n], pc1['PC1'][:n])
-        ax2.bar(pc2.index[:n], pc2['PC2'][:n])
-        ax3.bar(pc3.index[:n], pc3['PC3'][:n])
-
-        ax1.set_title('PC 1')
-        ax2.set_title('PC 2')
-        ax3.set_title('PC 3')
-
-        ax1.set_ylabel('Feature Importance')
-        ax2.set_ylabel('Feature Importance')
-        ax3.set_ylabel('Feature Importance')
-
-        ax1.tick_params(axis='x', rotation=90)
-        ax2.tick_params(axis='x', rotation=90)
-        ax3.tick_params(axis='x', rotation=90)
-
-        fig.tight_layout()
-        plt.close()
-        result['plot2'] = fig
-
-        # Store the top ASV loadings for each PC.
-        pd.set_option('display.max_colwidth', None)
-
-        ps1 = self.tax_table.loc[ pc1.index[:n] , : ]
-        ps2 = self.tax_table.loc[ pc2.index[:n] , : ]
-        ps3 = self.tax_table.loc[ pc3.index[:n] , : ]
-
-        ps1.insert(0, 'Loading', df.loc[pc1.index[:n], 'PC1'])
-        ps2.insert(0, 'Loading', df.loc[pc2.index[:n], 'PC2'])
-        ps3.insert(0, 'Loading', df.loc[pc3.index[:n], 'PC3'])
-
-        result['loading1'] = ps1
-        result['loading2'] = ps2
-        result['loading3'] = ps3
-
-        return result
 
     def subset(self, var, target):
         i = self.smp_table[var] == target
