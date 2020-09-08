d07efa2ba2c3fcc3e152c876dbc79dc85fa73039
SEUNG BEEN LEE <sbstevenlee@gmail.com>
2020-09-08 19:16:06 +0900

    Update jejudo.py

--- jejudo.py
+++ jejudo.py
@@ -11,15 +11,26 @@ import seaborn as sns
 
 TAXA = ['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
 
-def sdi(l):
+def shannon_diversity_index(l):
     def p(n, N):
         return (n/N) * np.log(n/N)
     N = sum(l)
     return -sum(p(n, N) for n in l if n)
 
+def simpson_diversity_index(l):
+    def p(n, N):
+        return (n/N)**2
+    N = sum(l)
+    return sum(p(n, N) for n in l if n)
+
+def inverse_simpson_diversity_index(l):
+    return 1 / simpson_diversity_index(l)
+
 def plot_richness(jjd, feature, measure='Shannon', ax=None, show=False,
                   figsize=None):
-    measures = {'Shannon': sdi}
+    measures = {'Shannon': shannon_diversity_index,
+                'Simpson': shannon_diversity_index,
+                'InverseSimpson': inverse_simpson_diversity_index}
 
     if not ax:
         fig, ax = plt.subplots(figsize=figsize)
