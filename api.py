dd8a9aed1db30ffaf4c3d504feba3db8ac5a2d2b
SEUNG BEEN LEE <sbstevenlee@gmail.com>
2020-09-22 09:36:02 +0900

    Add api.py

---
+++ api.py
@@ -0,0 +1,17 @@
+from tempfile import TemporaryDirectory
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from qiime2 import Visualization
+
+def ancom_volcano_plot(ancom, ax=None):
+    """This method creates an ANCOM volcano plot."""
+    t = TemporaryDirectory()
+    Visualization.load(ancom).export_data(t.name)
+    df = pd.read_table(f'{t.name}/data.tsv')
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(15, 10))
+    ax.scatter(df.clr, df.W, c='black', alpha=0.5)
+    ax.set_xlabel('clr')
+    ax.set_ylabel('W')
