18366bfb03961e8691c52f5170360617a2ab23ab
Seung-been Lee <sbstevenlee@gmail.com>
2020-12-16 13:57:37 +0900

    Update addbiplot() to display taxa names

--- api.py
+++ api.py
@@ -2146,10 +2146,13 @@ def addpairs(taxon,
 
 
 def addbiplot(pcoa_results,
+              taxonomy=None,
               dim=2,
               scale=1.0,
               count=5,
               fontsize=None,
+              name_type='feature',
+              level=None,
               ax=None,
               figsize=None):
     """
@@ -2160,6 +2163,9 @@ def addbiplot(pcoa_results,
     pcoa_results : str or qiime2.Artifact
         Artifact file or object corresponding to
         PCoAResults % Properties('biplot').
+    taxonomy : str or qiime2.Artifact
+        Artifact file or object corresponding to FeatureData[Taxonomy].
+        Required if `name_type='taxon'` or `name_type='confidence'.
     dim : [2, 3], default: 2
         Dimension of the input scatter plot.
     scale : float, default: 1.0
@@ -2168,6 +2174,11 @@ def addbiplot(pcoa_results,
         Number of important features to be displayed.
     fontsize : float or str, optional
         Sets font size.
+    name_type : ['feature', 'taxon', 'confidence'], default: 'feature'
+        Determines the type of names displayed. Using 'taxon' and 'confidence'
+        requires taxonomy.
+    level : int, optional
+        Taxonomic rank to be displayed. Only use with `name_type='taxon'`.
     ax : matplotlib.axes.Axes, optional
         Axes object to draw the plot onto, otherwise uses the current Axes.
     figsize : tuple, optional
@@ -2197,6 +2208,28 @@ def addbiplot(pcoa_results,
     feats.drop(['importance'], inplace=True, axis=1)
     feats = feats[:count]
 
+    if taxonomy is not None:
+        if isinstance(taxonomy, str):
+            _taxonomy = Artifact.load(taxonomy)
+        else:
+            _taxonomy = taxonomy
+        tax_df = _taxonomy.view(pd.DataFrame)
+        feats = pd.concat([feats, tax_df], axis=1, join='inner')
+
+    def f(s):
+        ranks = list(s.split(';'))
+        if level is None:
+            return s
+        else:
+            return ranks[level-1]
+
+    if name_type == 'feature':
+        names = feats.index
+    elif name_type == 'taxon':
+        names = [f(x) for x in feats['Taxon']]
+    else:
+        names = feats['Confidence']
+
     if ax is None:
         if dim == 2:
             fig, ax = plt.subplots(figsize=figsize)
@@ -2217,6 +2250,6 @@ def addbiplot(pcoa_results,
             b.append(z)
 
         ax.plot(*a, color='black')
-        ax.text(*b, feats.index[i], ha='center', fontsize=fontsize)
+        ax.text(*b, names[i], ha='center', fontsize=fontsize)
 
     return ax
