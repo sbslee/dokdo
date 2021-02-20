4340c70075811dd20f2709f392b9b223eaa99b0a
Seung-been Lee <sbstevenlee@gmail.com>
2021-02-20 17:10:31 +0900

    Add new method `pname()` (see #8)

--- dokdo/api.py
+++ dokdo/api.py
@@ -681,6 +681,38 @@ def ordinate(table,
 
 
 
+def pname(name):
+    """Return a prettified taxon name.
+
+    Parameters
+    ----------
+    name : str
+        Taxon name.
+
+    Returns
+    -------
+    str
+        Prettified name.
+    """
+    ranks = list(reversed(name.split(';')))
+    for i, rank in enumerate(ranks):
+        if rank in ['Others', 'Unassigned']:
+            return rank
+        if rank == '__':
+            continue
+        if rank.split('__')[1] is '':
+            return ranks[i+1] + ';' + rank
+        return rank
+
+
+
+
+
+
+
+
+
+
 # -- Main plotting methods ---------------------------------------------------
 
 def read_quality_plot(demux,
