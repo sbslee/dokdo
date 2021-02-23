10bfd3d3004f6828653b5d2651d9be998ccb01cb
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2021-02-23 17:23:02 +0900

    Add `mannwhitneyu()` which performs Mann–Whitney U test (see #8)

--- dokdo/api2/__init__.py
+++ dokdo/api2/__init__.py
@@ -1,5 +1,6 @@
 from .addpairs import addpairs
 from .wilcoxon import wilcoxon
+from .mannwhitneyu import mannwhitneyu
 from .num2sig import num2sig
 
-__all__ = ['addpairs', 'wilcoxon', 'num2sig']
+__all__ = ['addpairs', 'wilcoxon', 'mannwhitneyu', 'num2sig']
--- dokdo/api2/mannwhitneyu.py
+++ dokdo/api2/mannwhitneyu.py
@@ -2,10 +2,10 @@ import pandas as pd
 from scipy import stats
 from .num2sig import num2sig
 
-def wilcoxon(taxon, csv_file, subject, category, group1, group2, ann=False):
-    """Compute the p-value from the Wilcoxon Signed-rank test.
+def mannwhitneyu(taxon, csv_file, category, group1, group2, ann=False):
+    """Compute the p-value from the Mann–Whitney U test.
 
-    This method tests the null hypothesis that two related paired samples
+    This method tests the null hypothesis that two independent samples
     come from the same distribution for a given taxon.
 
     Parameters
@@ -14,8 +14,6 @@ def wilcoxon(taxon, csv_file, subject, category, group1, group2, ann=False):
         Target taxon name.
     csv_file : str
         Path to the .csv file from `dokdo.taxa_abundance_box_plot`.
-    subject : str
-        Column name to indicate pair information.
     category : str
         Column name to be tested.
     group1 : str
@@ -30,13 +28,15 @@ def wilcoxon(taxon, csv_file, subject, category, group1, group2, ann=False):
     -------
     float or str
         The p-value or signifiance annotation for the test
-        from `scipy.stats.wilcoxon`.
+        from `scipy.stats.mannwhitneyu`.
     """
     df = pd.read_csv(csv_file)
-    df = df.sort_values([subject, category])
+    df = df[[taxon, category]]
+    df = df.rename(columns={taxon: 'perc'})
+    df = df.sort_values([category])
     g1 = df[df[category] == group1]
     g2 = df[df[category] == group2]
-    p = stats.wilcoxon(g1[taxon], g2[taxon])[1]
+    p = stats.mannwhitneyu(g1['perc'], g2['perc'])[1]
     if ann:
         p = num2sig(p)
     return p
