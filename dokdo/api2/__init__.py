dc7275e4234d8dc347c7504f3f1950569fd0faec
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2021-02-24 13:24:06 +0900

    Fix bug with `alpha_diversity_plot()` (#8)

--- dokdo/api2/__init__.py
+++ dokdo/api2/__init__.py
@@ -1,6 +1,8 @@
+from .alpha_diversity_plot import alpha_diversity_plot
 from .addpairs import addpairs
 from .wilcoxon import wilcoxon
 from .mannwhitneyu import mannwhitneyu
 from .num2sig import num2sig
 
-__all__ = ['addpairs', 'wilcoxon', 'mannwhitneyu', 'num2sig']
+__all__ = ['alpha_diversity_plot', 'addpairs', 'wilcoxon',
+           'mannwhitneyu', 'num2sig']
