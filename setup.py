a92a09f1953de78d8df545e2fa94ed241dc50ddb
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2021-09-17 08:26:16 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,7 @@ setup(
                  "sequencing analysis with QIIME 2"),
     url="https://github.com/sbslee/dokdo",
     packages=find_packages(),
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
     entry_points={"console_scripts": ["dokdo=dokdo.__main__:main"]}
 )
